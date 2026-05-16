497fc1b62557a8db3bec02e95a9cc8c1fdf0a16d
Kiran Panchavati <kiranpanchavati9@gmail.com>
2026-05-16 21:32:51 +0400

    updated the tags for that

--- network/main.tf
+++ network/main.tf
@@ -11,11 +11,19 @@ resource "aws_security_group" "main" {
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.main.id
   cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
+  ip_protocol       = "-1" #
+
+  tags = {
+    Name = "roboshop-allow-all-traffic-egress"
+  }
 }
+  # semantically equivalent to all ports
 
 resource "aws_vpc_security_group_ingress_rule" "allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.main.id
   cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
+  ip_protocol       = "-1" #
+  tags = {
+    Name = "roboshop-allow-all-traffic-ingress"
+  }# sema# semantically equivalent to all ports
 }
