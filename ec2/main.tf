7fb112182b5a23fdc4667b640a4b72bd0cf9db69
Kiran Panchavati <kiranpanchavati9@gmail.com>
2026-05-17 14:50:44 +0400

    ansible module

--- ec2/main.tf
+++ ec2/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "main" {
   instance_type          = var.instance_type
   vpc_security_group_ids = var.vpc_sg_id
   iam_instance_profile   = var.iam_role
-
+  key_name = "roboshop-dev"
 
   tags = {
     Name        =  var.component_name
