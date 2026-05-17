6da163e311f21998b850bc2218e2b8205d7defd2
Kiran Panchavati <kiranpanchavati9@gmail.com>
2026-05-17 15:35:50 +0400

    ansible module

--- route53/main.tf
+++ route53/main.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "main" {
   zone_id = var.zone_id
-  name    = var.component_name
+  name    = "${var.component_name}-dev"
   type    = var.dns_type
   ttl     = var.ttl
   records = [var.private_ip]
